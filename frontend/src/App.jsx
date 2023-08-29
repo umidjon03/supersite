f5ff88c48c12e2d089401bf495116b7e6ba4b36c
shukhratovich-a <shukhratovich138@gmail.com>
2023-08-29 19:21:51 +0500

    Add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
 import { Layout } from "./layout/Layout";
 
+import { Home } from "./pages/home";
+
 const App = () => {
-  return <Layout>about</Layout>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Layout>
+  );
 };
 
 export default App;
