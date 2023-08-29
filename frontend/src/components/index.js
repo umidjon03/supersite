dcc201e13c70fb7c929a7e5ef19acedf5bde6140
shukhratovich-a <shukhratovich138@gmail.com>
2023-08-29 19:23:22 +0500

    Export components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,5 @@
 export * from "./Container/Container.component";
 
 export * from "./Switch/Switch.component";
+export * from "./Burger/Burger.component";
+export * from "./Button/Button.component";
