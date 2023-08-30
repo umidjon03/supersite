2471c0ebcc24d035284f72f122878361888670ed
sukhrob-py <sukhrobovlayev@gmail.com>
2023-08-30 11:49:52 +0500

    portfolio front added

---
+++ frontend/src/pages/portfolio.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Portfolio_Top from "../sections/Portfolio_Top/Portfolio_Top";
+import Portfolio_Project from "../sections/Portfolio_Project/Portfolio_Project";
+import { Container } from "../components";
+
+function Portfolio() {
+  return (
+    <div>
+      <Container>
+        <Portfolio_Top />
+      </Container>
+      <Portfolio_Project
+        title="lall"
+        order="1"
+        type="internet magzin"
+        img=""
+        summary=""
+      />
+    </div>
+  );
+}
+
+export default Portfolio;
