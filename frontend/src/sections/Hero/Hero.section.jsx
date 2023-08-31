cecdd4fd1ae14ed9dc5f178483a4ab6a52202068
shukhratovich-a <shukhratovich138@gmail.com>
2023-08-31 18:39:09 +0500

    Optimize image

--- frontend/src/sections/Hero/Hero.section.jsx
+++ frontend/src/sections/Hero/Hero.section.jsx
@@ -3,8 +3,8 @@ import cn from "classnames";
 
 import { Button, Container, Switch } from "../../components";
 
-import BulbOff from "../../assets/images/bulb-off.png";
-import BulbOn from "../../assets/images/bulb-on.png";
+import BulbOff from "../../assets/images/bulb-off.webp";
+import BulbOn from "../../assets/images/bulb-on.webp";
 
 import styles from "./Hero.module.scss";
 
