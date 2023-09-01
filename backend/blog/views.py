f8dc8bd3306d3e2c59ed435fb1a52018afd5ca01
sukhrob-py <sukhrobovlayev@gmail.com>
2023-09-01 08:10:44 +0500

    blog views fixed

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,4 +1,7 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.core.serializers import serialize
 
 from .models import BlogModel
 from .serializers import BlogSerializers
@@ -8,3 +11,19 @@ class BlogAllListApiView(ListAPIView):
     model = BlogModel
     serializer_class = BlogSerializers
     queryset = model.objects.all()
+
+
+class BlogOneListApiView(APIView):
+    def post(self, request):
+        yuid = request.data.get("yuid")
+        article = BlogModel.objects.filter(yuid=yuid)
+
+        return Response({"data": serialize("json", article)})
+
+
+class BlogListByCategoryApiView(APIView):
+    def post(self, request):
+        category = request.data.get("category")
+        articles = BlogModel.objects.filter(type=category)
+
+        return Response({"data": serialize("json", articles)})
