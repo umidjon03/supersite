0cc2d031d05ef9d1e49e1398f5ff180a9d7ea410
shukhratovich-a <shukhratovich138@gmail.com>
2023-09-04 02:30:55 +0500

    Edit

--- frontend/src/context/menu.context.jsx
+++ frontend/src/context/menu.context.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export const MenuContext = React.createContext(null);
 
 export const MenuProvider = ({ children }) => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
 
   return (
     <MenuContext.Provider value={{ isOpen, setIsOpen }}>
--- frontend/src/layout/Menu/Menu.jsx
+++ frontend/src/layout/Menu/Menu.jsx
@@ -1,17 +1,101 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
 import cn from "classnames";
-
-// import { useMenu } from "../../hooks/useMenu";
 
 import { Container } from "../../components";
 
 import styles from "./Menu.module.scss";
 
 export const Menu = () => {
-  // const { isOpen } = useMenu();
-
   return (
     <div className={cn(styles.menu)}>
-      <Container>Menu</Container>
+      <Container className={cn(styles.menu__container)}>
+        <ul className={cn(styles.menu__list)}>
+          <li className={cn(styles.menu__item)}>
+            <NavLink
+              className={({ isActive }) =>
+                cn(styles.menu__link, {
+                  [styles["menu__link--active"]]: isActive,
+                })
+              }
+              to="/portfolio"
+            >
+              Портфолио
+            </NavLink>
+          </li>
+          <li className={cn(styles.menu__item)}>
+            <NavLink
+              className={({ isActive }) =>
+                cn(styles.menu__link, {
+                  [styles["menu__link--active"]]: isActive,
+                })
+              }
+              to="/blog"
+            >
+              Блог
+            </NavLink>
+          </li>
+          <li className={cn(styles.menu__item)}>
+            <NavLink
+              className={({ isActive }) =>
+                cn(styles.menu__link, {
+                  [styles["menu__link--active"]]: isActive,
+                })
+              }
+              to="/marketing"
+            >
+              О маркетинге
+            </NavLink>
+          </li>
+        </ul>
+
+        <ul className={cn(styles.menu__small)}>
+          <li className={cn(styles.menu__item)}>
+            <NavLink
+              className={({ isActive }) =>
+                cn(styles.menu__link, {
+                  [styles["menu__link--active"]]: isActive,
+                })
+              }
+              to="/audit"
+            >
+              Разбор сайта
+            </NavLink>
+          </li>
+          <li className={cn(styles.menu__item)}>
+            <NavLink
+              className={({ isActive }) =>
+                cn(styles.menu__link, {
+                  [styles["menu__link--active"]]: isActive,
+                })
+              }
+              to="/qwiz1"
+            >
+              Квиз на разработку
+            </NavLink>
+          </li>
+          <li className={cn(styles.menu__item)}>
+            <NavLink
+              className={({ isActive }) =>
+                cn(styles.menu__link, {
+                  [styles["menu__link--active"]]: isActive,
+                })
+              }
+              to="/contacts"
+            >
+              Контакты
+            </NavLink>
+          </li>
+        </ul>
+
+        <div className={cn(styles.menu__info)}>
+          <span className={cn(styles.menu__info__link)}>
+            <a href="tel:+70663641919">+7(066) 364 19 19</a>
+            <span> / </span>
+            <a href="mailto:info@DidgiKey">info@DidgiKey</a>
+          </span>
+        </div>
+      </Container>
     </div>
   );
 };
