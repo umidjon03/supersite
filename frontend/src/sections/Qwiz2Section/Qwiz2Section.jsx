59c7dea27e4ad837c2c8f11b709fac76c5fea2f5
sukhrob-py <sukhrobovlayev@gmail.com>
2023-09-03 23:58:40 +0500

    qwiz2 edit

--- frontend/src/sections/Qwiz2Section/Qwiz2Section.jsx
+++ frontend/src/sections/Qwiz2Section/Qwiz2Section.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import Img1 from "../../assets/images/image 71qwiz2-1.png";
 import Img2 from "../../assets/images/img_blockqwiz2-2.png";
 import Img3 from "../../assets/images/image 71qwiz2-3.png";
+import Prev from "../../assets/images/Vector 15 (Stroke)left.png";
+import Next from "../../assets/images/Vector 15 (Stroke)right.png";
+
+import { Button } from "../../components";
 
 import "./qwiz2-section.scss";
 
 function Qwiz2Section() {
+  const [logo, setLogo] = useState("logo");
   return (
     <div className="qwiz2-section">
       <div className="cards">
@@ -15,7 +21,14 @@ function Qwiz2Section() {
             <img src={Img1} alt="" />
           </div>
           <div className="input">
-            <input type="radio" name="logo" id="style" />
+            <input
+              type="radio"
+              name="logo"
+              id="style"
+              onChange={() => {
+                setLogo("style");
+              }}
+            />
             <label htmlFor="style">Есть фир.стиль</label>
           </div>
         </div>
@@ -24,7 +37,14 @@ function Qwiz2Section() {
             <img src={Img2} alt="" />
           </div>
           <div className="input">
-            <input type="radio" name="logo" id="logo" />
+            <input
+              type="radio"
+              name="logo"
+              id="logo"
+              onChange={() => {
+                setLogo("logo");
+              }}
+            />
             <label htmlFor="logo">Есть логотип</label>
           </div>
         </div>
@@ -33,10 +53,35 @@ function Qwiz2Section() {
             <img src={Img3} alt="" />
           </div>
           <div className="input">
-            <input type="radio" name="logo" id="dont" />
+            <input
+              type="radio"
+              name="logo"
+              id="dont"
+              onChange={() => {
+                setLogo("nothing");
+              }}
+            />
             <label htmlFor="dont">Нет ничего</label>
           </div>
         </div>
+      </div>
+      <div className="btns">
+        <Link to="/qwiz1" className="link">
+          <Button className="prev">
+            <img src={Prev} alt="" /> Назад
+          </Button>
+        </Link>
+        <Link
+          to="/qwiz3"
+          className="link"
+          onClick={() => {
+            sessionStorage.setItem("logo", logo);
+          }}
+        >
+          <Button className="next">
+            Продолжить <img src={Next} alt="" />
+          </Button>
+        </Link>
       </div>
     </div>
   );
